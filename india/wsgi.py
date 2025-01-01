e632d348e677863827ef9c68b08228829a88eafc
arnel0069 <vikassharma00069@gmail.com>
2025-01-01 06:42:53 +0530

    Fix bad request issue

--- india/wsgi.py
+++ india/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'india' project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'india.settings')
 
+# Get the WSGI application for Django
 application = get_wsgi_application()
+
+# Alias `application` to `app` for Vercel compatibility
+app = application
